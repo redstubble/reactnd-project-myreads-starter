38a6e104e4dbaecab916617c41d026a5d332346f
John Williams <john@redstubble.com>
2017-10-09 16:08:35 +1300

    Looping through booksapi

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,2 +1,27 @@
+import React from 'react';
+//import PropTypes from 'prop-types'
+
+
 class ListBooks extends React.Component {
+    state = {
+        books: []
+      }
+
+    render() {
+        const { books } = this.props
+        return (
+            <div>
+                {books.map((book) => (
+                book.title
+              ))}
+                
+                </div>
+
+
+
+        )
+    }
+
+
 }    
+export default ListBooks
