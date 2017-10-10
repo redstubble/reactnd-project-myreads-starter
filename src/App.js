59e791e3d1b8cba99d861c32a71af94169b5dd3e
John Williams <john@redstubble.com>
2017-10-10 22:39:41 +1300

    Display page types - need to add functionality to change detail base on user interaction

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
+import { groupBy } from 'lodash'
 
 class BooksApp extends React.Component {
 
@@ -16,24 +17,66 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+// console.log(groupBy(books, 'shelf'))
+//       books.map((book) => (
+//       console.log("HELLO")
+//                 // console.log(book.shelf)
+//               ))
+
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-              this.setState({books: books})
-            })
+      this.setState({
+        books: groupBy(books, 'shelf')
+      })
+    })
+
   }
 
   render() {
+    let bookStatusTypes = Object.keys(this.state.books)
     return (
       <div className="app">
-        {/* Hello
-         {this.state.books.map((book) => (
-                "hjhj app"
-              ))}
-                  <script>
-                  console.log("hj" + {this.state.books})
-                  </script> */}
-                  <ListBooks books={this.state.books} />
-      </div>
+      {this.state.showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <div className="search-books-input-wrapper">
+              {/*
+                NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                You can find these search terms here:
+                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                you don't find a specific author or title. Every search is limited by search terms.
+              */}
+              <input type="text" placeholder="Search by title or author"/>
+
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+            {
+              bookStatusTypes.map((x, i) => 
+                <ListBooks key={i} bookStatus={x} books={this.state.books[x]} /> 
+                )
+            }
+            </div>
+          </div>
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
+        </div>
+      )}
+    </div>
     )
   }
 }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,27 +1,28 @@
 import React from 'react';
-//import PropTypes from 'prop-types'
-
+import PropTypes from 'prop-types'
+import Book from './Book'
 
 class ListBooks extends React.Component {
-    state = {
-        books: []
-      }
-
-    render() {
-        const { books } = this.props
-        return (
-            <div>
-                {books.map((book) => (
-                book.title
-              ))}
-                
-                </div>
-
-
-
-        )
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        bookStatus: PropTypes.string.isRequired
     }
 
-
-}    
+    render() {
+        const { books, bookStatus } = this.props
+        return (
+             <div className="bookshelf">
+                <h2 className="bookshelf-title">{bookStatus
+                .replace(/([A-Z])/g, ' $1')
+                .replace(/^./, function(str){ return str.toUpperCase(); })}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid"> {
+                      books.map((m, i) => <Book key={i} book={m} /> )
+                      }
+                  </ol>
+                </div>
+              </div>
+        )
+    }
+}
 export default ListBooks
